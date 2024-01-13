bfd5540d485512e1a06a9c028fd873af40e875a6
Trey Jones <141204905+gjones94@users.noreply.github.com>
2024-01-13 00:52:45 -0600

    Add Bubble Sort Add Insertion Sort

--- Algorithms/Sorting/Sort.py
+++ Algorithms/Sorting/Sort.py
@@ -1,4 +1,27 @@
 
-
 def bubble_sort(arr):
-    print(arr)
+    print("Bubble Sort")
+    print("O(n^2)")
+    for i in range(0, len(arr) - 1):
+        for j in range(0, len(arr) - 1):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+
+            print(f'{(i * len(arr)) + j}: {arr}')
+            
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        for j in range(i-1, -1, -1):
+            if arr[j] > arr[j+1]: # swap greater element to the right
+                temp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = temp
+
+            print(f'{(i * len(arr)) + j}: {arr}')
+            
+
+            
+    
+                    
---
+++ Algorithms/main.py
@@ -0,0 +1,9 @@
+
+import Sorting.Sort as sort
+
+def main():
+    elements = [1, 6, 5, 8, 3, 9, 0]
+    #sort.bubble_sort(elements)
+    sort.insertion_sort(elements)
+    
+main()
